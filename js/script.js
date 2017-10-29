cd0366cfd8ba9f3ec085fb1d55681b51b2572a9a
Yulia Buzina <buzinay@mail.ru>
2017-10-29 13:10:10 +0300

    Внесла правки

--- js/script.js
+++ js/script.js
@@ -1,21 +1,16 @@
-/*var navPage = document.querySelector('.page-header__nav');*/
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function() {
-    if (navMain.classList.contains('main-nav--closed')) {
-          navMain.classList.remove('main-nav--closed');
-          navMain.classList.add('main-nav--opened');
-          /*navPage.classList.remove('page-header__nav--closed');
-          navPage.classList.add('page-header__nav--opened');*/
-        } else {
-            navMain.classList.add('main-nav--closed');
-            navMain.classList.remove('main-nav--opened');
-            /*navPage.classList.add('page-header__nav--closed');
-            navPage.classList.remove('page-header__nav--opened');*/
-          }
+  if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+    }
 });
 
 var myMap;
@@ -23,7 +18,7 @@ var myPlacemark;
 ymaps.ready(init);
   function init () {
     myMap = new ymaps.Map('map-id', {
-        center: [59.9386,30.32],
+        center: [59.938631,30.323055],
         zoom: 15,
         controls: ["zoomControl", "fullscreenControl"]
   });
